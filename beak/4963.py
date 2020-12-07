a817940a6ad57c601ee33dcfb7d1cd55f82c946d
yeonju <apple2062@naver.com>
2020-12-07 12:31:41 +0900

    섬의 개수

--- beak/4963.py
+++ beak/4963.py
@@ -1,37 +1,37 @@
-#4963 (섬의개수)
-#섬의 개수를 세는 프로그램
-#한 정사각형과 가로, 세로 또는 대각선으로 연결되어 있는 사각형은 걸어갈 수 있는 사각형이다.
-#이 문제의 핵심은"대각선"도 걸어갈 수 있다는 것!!!!!
+# 4963 (섬의개수)
+# 섬의 개수를 세는 프로그램
+# 한 정사각형과 가로, 세로 또는 대각선으로 연결되어 있는 사각형은 걸어갈 수 있는 사각형이다.
+# 이 문제의 핵심은"대각선"도 걸어갈 수 있다는 것!!!!!
+import sys
+sys.setrecursionlimit(10**7)
 
-def solution(matrix,w,h):
+dx = [-1, -1, 1, 1, -1, 0, 1, 0]
+dy = [-1, 1, 1, -1, 0, 1, 0, -1]
+answer = []
+
+def solution(x, y):  # dfs
+    visited[x][y] = 1
+    for i in range(8):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0<=nx<h and 0<=ny<w and matrix[nx][ny]==1 and visited[nx][ny]==0:
+            solution(nx, ny)
+while True:
+    w, h = map(int, input().split())  # 너비 w, 높이 h
+    if w == 0 and h == 0:
+        break
+    matrix = []
     cnt = 0
-    visited = [[0]*w for _ in range(h)]
-    #print(visited)
+    visited = [[0] * w for _ in range(h)]
+    for _ in range(h):
+        matrix.append(list(map(int, input().split())))
     for i in range(h):
         for j in range(w):
-            if matrix[i][j] == 1 and visited[i][j]==0:
-                visited[i][j] = 1
-                for k in range(8):
-                    x = i+dx[k]
-                    y = j+dy[k]
-                    if 0<=x<h and 0<=y<w and matrix[x][y]==1 and visited[x][y]==0:
-                        visited[x][y] = 1
-            cnt+=1
+            if matrix[i][j] == 1 and visited[i][j] == 0:
+                solution(i, j)
+                cnt += 1
     answer.append(cnt)
 
-answer = []
-dx = [-1,0,1,0,-1,-1,1,1] #대각선 포함
-dy = [0,-1,0,-1,-1,1,-1,1]
-
-
-while True:
-    matrix = []
-    w,h = map(int,input().split())
-    if w==0 and h==0:
-        break
-    for i in range(h):
-        matrix.append(list(map(int,input().split())))
-    solution(matrix,w,h)
 
 for i in answer:
     print(i)
