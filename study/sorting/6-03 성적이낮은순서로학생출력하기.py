37d37d52820937df3890d74e29c334c1687b572b
yeonju <apple2062@naver.com>
2021-01-12 23:58:03 +0900

    .

--- study/sorting/6-03 성적이낮은순서로학생출력하기.py
+++ study/sorting/6-03 성적이낮은순서로학생출력하기.py
@@ -1,3 +1,12 @@
-:wq
+# 6-3 성적이 낮은 순서로 학생 출력하기
+
+n = int(input())
+matrix = []
+for i in range(n):
+    name,score = map(str,input().split())
+    matrix.append((name,int(score)))
+matrix = sorted(matrix,key = lambda x:x[1])
+for i in matrix:
+    print(i[0], end = ' ')
 
 
