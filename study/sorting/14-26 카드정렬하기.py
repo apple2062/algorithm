6ae527b2b1cb56a5460fe5715003f7646efac290
yeonju <apple2062@naver.com>
2021-01-13 23:52:52 +0900

    heapq

---
+++ study/sorting/14-26 카드정렬하기.py
@@ -0,0 +1,19 @@
+# 26 카드 정렬하기
+import heapq
+import sys
+input = sys.stdin.readline
+
+n = int(input()) #숫자 카드 묶음 개수
+answer = 0
+matrix = []
+
+for i in range(n):
+    heapq.heappush(matrix,int(input()))
+
+while matrix:
+    if len(matrix) == 1:
+        print(answer)
+        break
+    value = heapq.heappop(matrix)+heapq.heappop(matrix)
+    answer += value
+    heapq.heappush(matrix,value)
