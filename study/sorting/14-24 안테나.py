422de221a4df3811ac9852dbab9844650421bdae
yeonju <apple2062@naver.com>
2021-01-13 15:56:53 +0900

    첫 시도 시간초과

--- study/sorting/14-24 안테나.py
+++ study/sorting/14-24 안테나.py
@@ -1,20 +1,12 @@
 # 안테나
+# 시간 복잡도 O(N log N)
+'''
+첫 시도 시간초과로 실패 
+해결 포인트 : 중간값에 해당하는 위치의 집에 안테나를 설치했을 때, 거리 총합이 최소가 된다는 점
+'''
 
-#특정 위치의 집에 특별히 한 개의 안테나를 설치 , 안테나로부터 모든 집까지의 거리의 총 합이 최소가 되도록 설치
-#논리적으로 동일한 위치에 여러 개의 집이 존재하는 것이 가능
-
-import sys
 n = int(input()) # 집의 수
 matrix = list(map(int,input().split())) #집의 위치
-minimum = sys.maxsize
-house = 0
+matrix.sort()
+print(matrix[(n-1)//2])
 
-for i in range(n):
-    answer = 0
-    for j in matrix:
-        answer += abs(matrix[i]-j)
-    if minimum > answer :
-        minimum = answer
-        house = matrix[i]
-        
-print(house)
