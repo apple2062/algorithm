626aac382138d8dbecc7e11a32b8ffb1bd720819
yeonju <apple2062@naver.com>
2021-02-01 19:40:07 +0900

    동전1 (2293)과 유사

--- study/dp/8-05 효율적인화폐구성.py
+++ study/dp/8-05 효율적인화폐구성.py
@@ -1,23 +1,23 @@
 # 효율적인 화폐 구성
+# n 가지 종류의 화폐로 m 원이 되도록 만들기
 
 import inf
 
-def solution(lst):
-    dp = [0] + [inf] * m
-    for i in range(n): # 각각의 화폐 단위에 따른
-        for j in range(lst[i],m+1): # 해당 금액부터 시작한 모든 금액을 확인해 보며
-            if dp[j-lst[i]] != inf:
-                dp[j] = min(dp[j],dp[j-lst[i]]+1) # 더 작은값 갱신
-
-    if dp[m] == inf:
-        return -1
-    else:
-        return dp[m]
-
 n,m = map(int,input().split())
-lst = []
+currency = []
 for i in range(n):
-    lst.append(int(input()))
+      currency.append(int(input()))
 
-print(solution(lst))
+dp= [inf]*(m+1)
 
+dp[0]=0
+
+for i in range(n):
+      for j in range(currency[i], m+1):
+            if dp[j-currency[i]] != inf:
+                  dp[j] = min(dp[j], dp[j-currency[i]]+1)
+
+if dp[m] == inf:
+      print(-1)
+else:
+      print(dp[m])
