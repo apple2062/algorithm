99fde06e3498a2857c713e63bc6991b509e694a0
yeonju <apple2062@naver.com>
2021-02-11 20:03:33 +0900

    ref/second try

--- study/graph/알파벳(1987).py
+++ study/graph/알파벳(1987).py
@@ -1,39 +1,41 @@
 # 알파벳 (1987) - https://www.acmicpc.net/problem/1987
-''' (실패코드)
-첫번째 시도: dfs 인 것 같은데...
-          아래 예시,
+'''
+두번째 시도: TLE in python3. pypy3로 통과했다. (dfs)
+          tc1.
           2 2
           AB
-          CA
-          에 대해 2가 아닌 3이 나왔다.
-'''
-# 새로 이동한 칸에 적힌 알파벳은 지금껏 지나온 알파벳과는 달라야 한다. 즉, 같은 알파벳 칸은 두번 지날 수 없음
-# 말이 최대한 몇 칸을 지날 수 있는지 구하는 프로그램 작성
+          CA 에 대해 2가 아닌 3이 나왔다.
 
-def dfs(x,y):
-    visited[x][y] = 1
+          tc2.
+          4 4
+          AAAA
+          BCDE
+          CAAA
+          DEFG 에 대해 7 이 아닌 5가 나왔다. >> 이미 storage 에 b,c,d,e 가 있어서 아래로 b,c,d,e를 가지 못했던 것.
+                                          깊게 들어간 곳이 끝에 다다르면 storage 에서 빼고 그 다음 단계를 가야했다.
+'''
+
+def dfs(x,y,cnt):
+    global cnt__
+    cnt__ = max(cnt__,cnt)
     for i in range(4):
         nx = x+ dx[i]
         ny = y+ dy[i]
         if 0<=nx<r and 0<=ny<c:
-                if visited[nx][ny]==0:
                     if matrix[nx][ny] not in storage:
                         storage.append(matrix[nx][ny])
-                        dfs(nx,ny)
-    return len(storage)
+                        dfs(nx,ny,cnt+1)
+                        storage.remove(matrix[nx][ny])
+    return cnt__
 
 r,c = map(int,input().split()) #세로, 가로
 matrix = []
-visited = [[0]*c for _ in range(r)]
+cnt__ = 1
 dx = [-1,0,1,0]
 dy = [0,1,0,-1]
-
-
 for i in range(r):
     matrix.append(list(input()))
-
 storage = [matrix[0][0]] # 알파벳 담을 저장소
-
-print(dfs(0,0))
+print(dfs(0,0,1)) #(x,y,cnt)
 
 
