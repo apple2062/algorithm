f00e7ba713c073b86a49328b6105d37fee176940
yeonju <apple2062@naver.com>
2021-03-05 19:42:11 +0900

    target을두고순회

--- study/week11/빗물(14719).py
+++ study/week11/빗물(14719).py
@@ -1,9 +1,8 @@
 # 빗물 ( https://www.acmicpc.net/problem/14719 )
 '''
-1.taregt이라고 둔 높이마다 순회하면서
-2.벽 발견 시(target 과 값이 크거나 같은 경우)  그때부터 카운트 1 해준다
-3.최초 벽 다음으로 벽을 발견하면 카운트 갱신해주고
-    4. 만일 최초 벽 다음으로 벽을 발견하지 못한다면 카운트는 갱신하지 않고 0으로 초기화
+1.taregt이라고 둔 기준 높이마다 순회하면
+2.벽 발견 시(target 과 값이 크거나 같은 경우) 벽 인덱스를 lst에 저장해준다.
+3.lst 길이가 1이상인 경우, 저장한 인덱스를 순회하며 인덱스 차이만큼 값을 더해준다.
 '''
 # 고이는 빗물의 총량
 
@@ -13,19 +12,16 @@ matrix = list(map(int,input().split())) # [3,1,2,3,4,1,1,2]
 rain = 0 # 빗물의 총량
 
 for target in range(h,0,-1):
-    lst = [0]*(w)
-    lst1 = []
+    lst = [] # target보다 높이가 같거나 큰 벽을 만난 경우, 해당 벽의 인덱스를 저장하기 위함
     cnt = 0
     for i in range(w):
         if target<=matrix[i]:
-            lst[i] = 1
-            lst1.append(i+1)
-    if len(lst1)!= 1:
-        for i in range(len(lst1)-1):
-            cnt += (lst1[i+1]-lst1[i]-1)
+            lst.append(i)
+    if len(lst)!= 1: #lst의 길이가 1 이면, 빗물을 감쌀 수 없기 때문에 배제
+        for i in range(len(lst)-1):
+            cnt += (lst[i+1]-lst[i]-1)
     rain+=cnt
 print(rain)
-
 
 
 
