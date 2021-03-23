25a215637039c541262ad49c5e4b8332257bb929
yeonju <apple2062@naver.com>
2021-03-23 18:18:33 +0900

    TLE

---
+++ study/week13/탑(2493).py
@@ -0,0 +1,21 @@
+# 탑 ( https://www.acmicpc.net/problem/2493 )
+
+
+def solution(matrix):
+    answer = []
+    matrix.reverse()
+    for i in range(n-1):
+        for j in range(i+1,n):
+            if matrix[i]<matrix[j]:
+                #print(i,matrix[i],matrix[j])
+                answer.append(5-matrix.index(matrix[j]))
+                break
+            if j == (n-1):
+                answer.append(0)
+    answer.append(0) #이 부분 다시 작업하고 난뒤에 answer 다시 reverse 해주기
+    return answer
+
+
+n = int(input()) #탑의 수
+matrix = list(map(int,input().split()))
+print(solution(matrix))
