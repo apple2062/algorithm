52acc5024d5527c5cb004ad8a3c256baf09d448d
yeonju <apple2062@naver.com>
2021-03-26 12:28:39 +0900

    dp+greedy

---
+++ programmers/[L3]정수삼각형.py
@@ -0,0 +1,16 @@
+# https://programmers.co.kr/learn/courses/30/lessons/43105
+
+def solution(triangle):
+    for i in range(1,len(triangle)):
+        for j in range(len(triangle[i])):
+            # 맨 왼쪽에 있는 경우
+            if j == 0:
+                triangle[i][j] += triangle[i-1][j]
+            elif j == len(triangle[i])-1:
+                triangle[i][j] += triangle[i-1][j-1]
+            else:
+                triangle[i][j] += max(triangle[i-1][j-1],triangle[i-1][j])
+    return max(triangle[-1])
+
+triangle = [[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]] # return 30
+print(solution(triangle))
