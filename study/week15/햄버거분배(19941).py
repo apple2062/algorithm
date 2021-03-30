41db169a5279bbfde29a8685441563fdd8275c2c
yeonju <apple2062@naver.com>
2021-03-30 20:03:10 +0900

    .

--- study/week15/햄버거분배(19941).py
+++ study/week15/햄버거분배(19941).py
@@ -1,22 +1,28 @@
 # 햄버거 분배 ( https://www.acmicpc.net/problem/19941 )
 
+# P(사람)와 H(햄버거)
 import sys
-N, K = list(map(int, sys.stdin.readline().split()))
-table = sys.stdin.readline().strip()
+input = sys.stdin.readline
 
 def solution():
-    eat = [0] * N
     answer = 0
-    for i in range(N):
-        if table[i] == 'P':
-            for offset in range(-K, K + 1):
-                idx = i + offset
-                if 0 <= idx < N and table[idx] == 'H' and not eat[idx]:
-                    eat[idx] = 1
-                    answer += 1
-                    break
+    visited = [False]*n
+    for i in range(n):
+        if info[i] == 'P':
+            for j in range(-k,k+1):
+                idx = i+j
+                if 0<=idx<n and info[idx] == 'H':
+                    if not visited[idx]:
+                        visited[idx] = True
+                        answer += 1
+                        break
     return answer
+
+n,k = map(int,input().split())
+info = input()
+
 print(solution())
+
 
 """
 20 1
